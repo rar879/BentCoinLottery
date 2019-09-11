a05f0c453a16cc21dac9212d53c8d4dfd6bc1bb4
Rob Rodriguez <rrodriguez4@uchicago.edu>
2019-09-11 14:41:47 -0400

    Updated Code
    Added clarity to the variable names

--- lottery.R
+++ lottery.R
@@ -11,15 +11,22 @@ library(stats)
 
 # Small edit! from class 1!
 
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+number_of_coin_flips <- 10L
+probability_of_heads <- 0.1L
+max_number_repeated_coin_flip_iterations <- 100L
+iteration <- i
+
+
+Outcome_of_Coin_Flips <- array(0, number_of_coin_flips)
+for (iteration in 1:10){
+  #random number within a uniform distribution
+  Outcome_of_Coin_Flips[iteration] <- if (runif(1) < probability_of_heads) 1 else 0
 }
 
-for (j in 1:100){
-  arr <- array(0,10)
-  for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
+for (Number_Repeat_Iterations_of_10_Coin_Flips in 1:max_number_repeated_coin_flip_iterations){
+  Outcome_of_Coin_Flips <- array(0,10)
+  for (iteration in 1:10){
+    Outcome_of_Coin_Flips[iteration] <- if (runif(1) < probability_of_heads) 1 else 0
   }
-  print(arr)
+  print(Outcome_of_Coin_Flips)
 }
