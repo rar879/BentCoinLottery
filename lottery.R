840b366261210a0b6a08cde5617a50cf1e6bc2a9
Rob Rodriguez <rrodriguez4@uchicago.edu>
2019-09-11 15:09:47 -0400

    Changed 10 values for iterations
    changed from [...,10] to the defined variable for 10

--- lottery.R
+++ lottery.R
@@ -18,13 +18,13 @@ iteration <- i
 
 
 Outcome_of_Coin_Flips <- array(0, number_of_coin_flips)
-for (iteration in 1:10){
+for (iteration in 1:number_of_coin_flips){
   #random number within a uniform distribution
   Outcome_of_Coin_Flips[iteration] <- if (runif(1) < probability_of_heads) 1 else 0
 }
 
 for (Number_Repeat_Iterations_of_10_Coin_Flips in 1:max_number_repeated_coin_flip_iterations){
-  Outcome_of_Coin_Flips <- array(0,10)
+  Outcome_of_Coin_Flips <- array(0,number_of_coin_flips)
   for (iteration in 1:10){
     Outcome_of_Coin_Flips[iteration] <- if (runif(1) < probability_of_heads) 1 else 0
   }
